dc30ae4c6f0e6c4ee0df2960365cbda06e72100b
Yonela <yonelamnagele1700@gmail.com>
2024-08-20 10:54:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import userRouter from './routes/userRouter.js'
+import productRouter from './routes/productRouter.js'
+
+let port = process.env.PORT || 2004
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/users', userRouter)
+app.use('/products', productRouter)
+
+app.listen(port, () => {
+    console.log('http://localhost:' +port)
+})
+
+
+
