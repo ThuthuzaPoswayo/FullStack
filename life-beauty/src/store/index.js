1ea39a341c1c8b67568cf689313d0bb20af25b40
ThuthuzaPoswayo <thuthuzaposwayo@gmail.com>
2024-08-21 12:01:23 +0200

    frontend

---
+++ life-beauty/src/store/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+    // component: () =>import('@/views/HomeView.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+export default router
