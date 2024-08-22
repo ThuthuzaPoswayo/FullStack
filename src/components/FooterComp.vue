a5f86a2380f7c80b61c3aa1ac5c82daa6b53c714
ThuthuzaPoswayo <thuthuzaposwayo@gmail.com>
2024-08-22 09:43:44 +0200

    Footer

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+        <p class="lead">
+            Ytee Beauty &copy; <span id="currYear"></span>
+        </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
