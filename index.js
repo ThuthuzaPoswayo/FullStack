60aafaf4d367c2686f85946504087743335ced83
Yonela <yonelamnagele1700@gmail.com>
2024-08-23 09:49:15 +0200

    sixth commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:8083',
     credentials: true
 }))
 
