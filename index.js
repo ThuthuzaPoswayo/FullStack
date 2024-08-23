1838d521d2b4d1524fb7cbb510464602e0b68289
Yonela <yonelamnagele1700@gmail.com>
2024-08-23 13:56:42 +0200

    eighth commit

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ let port = process.env.PORT || 2004
 
 const app = express()
 
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next()
-})
-
 app.use(cors({
     origin: 'http://localhost:8083',
     credentials: true
