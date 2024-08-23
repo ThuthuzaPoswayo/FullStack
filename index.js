2652e122be0c995cf22d8337f48fe924cac68dad
ThuthuzaPoswayo <167189199+ThuthuzaPoswayo@users.noreply.github.com>
2024-08-23 10:32:32 +0200

    Update index.js
    adding the cors middleware

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ let port = process.env.PORT || 2004
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
+app.use(cors())
+
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cors({
